15a8ea75f2d9e478498c9a205d3881d8f9aae08c
yumeinaruu <lisovoistas@yandex.ru>
2023-10-12 21:51:48 +0300

    end commit

--- src/pair4/Task1.java
+++ src/pair4/Task1.java
@@ -1,6 +1,7 @@
 package pair4;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task1 {
@@ -30,12 +31,60 @@ public class Task1 {
         System.out.println(array[1]);*/
 
 
-        String[] fruits = new String[4];
+/*        String[] fruits = new String[4];
         for (int i = 0; i < fruits.length; i++) {
             fruits[i] = scanner.nextLine();
         }
         System.out.println(fruits[1] + " " + fruits[2] + "\n" + fruits.length);
         fruits[2] = scanner.nextLine();
-        System.out.println(fruits[2]);
+        System.out.println(fruits[2]);*/
+
+/*        int[] nums = new int[5];
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = (int)((Math.random() * 100));
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+        }
+
+        String lineArray = Arrays.toString(nums);
+        System.out.println(lineArray);
+        Arrays.sort(nums);
+
+        String lineArray1 = Arrays.toString(nums);
+        System.out.println(lineArray1);*/
+
+/*        int[] nums = new int[5];
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = (int)((Math.random() * 100));
+        }
+        int[] secondNums = new int[10];
+        secondNums = Arrays.copyOf(nums, nums.length);
+
+        System.out.println(Arrays.toString(secondNums));
+
+        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.binarySearch(nums, 23));*/
+
+        int len = scanner.nextInt();
+        double[] masDouble = new double[len];
+
+        for (int i = 0; i < masDouble.length; i++) {
+            masDouble[i] = (Math.random() * 100);
+            System.out.println(masDouble[i]);
+            if(masDouble[i] % 2 != 0){
+                masDouble[i] = Math.pow(masDouble[i], 2);
+            }
+        }
+
+        System.out.println(Arrays.toString(masDouble));
+
+        System.out.print("\n[");
+
+        for (int i = (masDouble.length - 1); i >= 0; i--) {
+            System.out.print(masDouble[i] + " ");
+        }
+        System.out.println("]");
     }
 }
