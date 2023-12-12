c6c5e11df7e4c444ceb5bdd765561df52d05b1ac
Yumeinaruu <lisovoistas@yandex.ru>
2023-12-12 19:36:55 +0300

    Initial Commit

---
+++ src/pair20/test/Store.java
@@ -0,0 +1,26 @@
+package pair20.test;
+
+public class Store {
+    private int product = 0;
+
+    public synchronized void get() throws InterruptedException {
+        while (product < 1) {
+            wait();
+        }
+        product--;
+        System.out.println("Покупатель купил 1 товар");
+        System.out.println("Товаров на складе: " + product);
+        notify();
+    }
+
+    public synchronized void put() throws InterruptedException {
+        while (product >= 3) {
+            wait();
+        }
+        product++;
+        System.out.println("Производитель добавил 1 товар");
+        System.out.println("Товаров на складе: " + product);
+        notify();
+    }
+
+}
